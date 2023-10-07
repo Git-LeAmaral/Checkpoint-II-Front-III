90f7c41608630c1c5fb0ff7025b9ec383bf02bfb
cassiotsantos <psyteqkilla@gmail.com>
2023-10-06 22:23:10 -0300

    configurando autenticação da página login

--- Checkpoint_II/src/Components/LoginForm/LoginForm.jsx
+++ Checkpoint_II/src/Components/LoginForm/LoginForm.jsx
@@ -1,11 +1,19 @@
 
 import styles from "./Form.module.css";
+import { useContext, useState } from 'react';
+import { api } from '../../services/api';
+import { useNavigate } from 'react-router-dom';
+import { OdontoContext } from '../../contexts/OdontoContext';
 
 
 export const LoginForm = () => {
 
+  const [errorForm, setErrorForm] = useState(false);
+  const { darkMode, login } = useContext(OdontoContext);
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+
+  const handleSubmit = async (e) => {
     //Nesse handlesubmit você deverá usar o preventDefault,
     //enviar os dados do formulário e enviá-los no corpo da requisição 
     //para a rota da api que faz o login /auth
@@ -14,7 +22,31 @@ export const LoginForm = () => {
     //Com tudo ocorrendo corretamente, o usuário deve ser redirecionado a página principal,com react-router
     //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
     e.preventDefault();
-  
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+
+    try {
+      const response = await api.post('/auth', 
+      {
+        username: data.login,
+        password: data.password,
+      }, {
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+      });
+
+      if (response.status === 200) {
+        localStorage.setItem('token', response.data.token);
+        login();
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorForm(true)
+      console.log(errorForm);
+      alert("Não foi possivel realizar seu login! \n Verifique seu usuário e senha e tente novamente.")}
+ 
   };
 
   return (
@@ -22,9 +54,9 @@ export const LoginForm = () => {
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar o css correto */}
       <div
-        className={`text-center card container ${styles.card}`}
+        className={`text-center card container ${styles.card} ${darkMode ? `light-card bg-light` : `dark-card bg-dark`}`}
       >
-        <div className={`card-body ${styles.CardBody}`}>
+        <div className={`card-body ${styles.CardBody} ${darkMode ? `light-card bg-light` : `dark-card bg-dark`} `}>
           <form onSubmit={handleSubmit}>
             <input
               className={`form-control ${styles.inputSpacing}`}
@@ -39,6 +71,7 @@ export const LoginForm = () => {
               type="password"
               required
             />
+            {errorForm ? (<small className={styles.error}>Opps! Alguma coisa errada.</small>) : (<></>)}
             <button className="btn btn-primary" type="submit">
               Send
             </button>
